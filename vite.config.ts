e2486032d340362e6205d0f2e33dcebfd76c80a2
Mykhailo Sulyma <msulima@swivl.com>
2026-03-29 12:52:14 +0300

    Move allowedHosts to server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  allowedHosts: [
-    'mywallet.home'
-  ],
+  server: {
+    allowedHosts: [
+      'mywallet.home'
+    ]
+  },
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
   test: {
